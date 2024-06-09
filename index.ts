834cccbc3a212836459dc429ae66a64ecf2c4d2b
abraham-ukachi <abraham.ukachi@laplateforme.io>
2024-06-09 19:37:04 +0200

    :bug: Fix publish to npm workflow issues

---
+++ index.ts
@@ -0,0 +1,74 @@
+/* 
+* @license MIT
+* ~~~~~~~~~~~~
+* ab-nextjs-fonts
+* ~~~~~~~~~~~~ 
+* Copyright (c) 2024 Abraham Ukachi. The abElements Project.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the 'Software'), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions: 
+*  
+* The above copyright notice and this permission notice shall be included in all 
+* copies or substantial portions of the Software. 
+*
+* THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* @project: ab-nextjs-fonts
+* @name: Fonts - abElements
+* @file: index.ts
+* @type: TypeScript 
+* @authors: Abraham Ukachi <abraham.ukachi@laplateforme.io>
+*
+* Example usage:
+*   1+|> // Import the inter styles  
+*    -|> import { interStyles } from 'ab-nextjs-fonts';
+*    -|> 
+*
+*/
+
+
+
+/*
+* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+* MOTTO: We'll always do more 😜!!!
+* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
+
+
+
+
+import interStyles from './inter/styles.css';
+// TODD: import mulishStyles from './mulish/styles.css';
+// TODD: import quicksandStyles from './quicksand/styles.css';
+// TODO: import robotoStyles from './roboto/styles.css';
+// TODO: import zillaSlabStyles from './zilla-slab/styles.css';
+
+
+
+// TODO: do something w/ the `interStyles` here ;)
+// TODO: do something w/ the `mulishStyles` here ;)
+// TODO: do something w/ the `quicksandStyles` here ;)
+// TODO: do something w/ the `robotoStyles` here ;)
+// TODO: do something w/ the `zillaSlabStyles` here ;)
+
+
+// exporting all the styles...
+
+export {
+  interStyles,
+  // TODO: mulishStyles,
+  // TODO: quicksandStyles,
+  // TODO: robotoStyles,
+  // TODO: zillaSlabStyles,
+
+};
